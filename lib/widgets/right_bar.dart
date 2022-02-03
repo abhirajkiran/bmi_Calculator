d6396aba0c7e3ead55cf26a48452d34b09f26312
Abhiraj Kiran <abhirajkiran100@gmail.com>
2022-02-03 03:21:51 -0800

    finished

---
+++ lib/widgets/right_bar.dart
@@ -0,0 +1,32 @@
+//import 'package:weight_app/constants/app_constant.dart';
+import 'package:flutter/material.dart';
+
+class rightBar extends StatelessWidget {
+
+
+final double barWidth;
+
+  const rightBar({Key? key,required this.barWidth}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration:  BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              bottomRight: Radius.circular(20)
+            ),
+            color: Colors.yellow
+          ),
+        )
+      ],
+      
+    );
+  }
+}
